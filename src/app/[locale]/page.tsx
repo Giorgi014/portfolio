66bda24dac3e8d76286e123536f62f05c99338d8
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-12-12 22:20:03 +0400

    feat: Integrate i18n setup for multilingual support

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,5 @@
 import HomePage from "./home/page";
-import ParticleBackground from "../components/particleBackground";
+import ParticleBackground from "@/components/particleBackground";
 
 export default function Home() {
   return (
