2e0795d26aa7132bbc738e07b061655dcaa28d7c
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-12-30 00:54:41 +0400

    feat: Add settings section to sidebar and apply initial styling

--- src/app/[locale]/home/sidebar/page.tsx
+++ src/app/[locale]/home/sidebar/page.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { IoSettingsSharp } from "react-icons/io5";
+import "./style/sidebar.scss";
 
 const Sidebar = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
+    <article className="sidebar">
       {children}
-      <IoSettingsSharp />
-    </div>
+      <section className="sidebar_settings">
+        <button className="settings_btn">
+          <p className="settings_text">Settings</p>
+          <IoSettingsSharp className="settings_icon" />
+        </button>
+      </section>
+    </article>
   );
 };
 
