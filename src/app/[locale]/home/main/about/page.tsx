f95be10edbccb8bad290e93dc2eb5505ed1362ba
Giorgi014 <giorgi.gugunava99@gmail.com>
2026-01-10 19:23:01 +0400

    feat: Add expand and collapse animation for (About Me) text

--- src/app/[locale]/home/main/about/page.tsx
+++ src/app/[locale]/home/main/about/page.tsx
@@ -7,12 +7,88 @@ import { SectionProps } from "@/components/type";
 import { useTranslations } from "next-intl";
 import { RiCloseLargeFill } from "react-icons/ri";
 import { useToggleAnimation } from "@/app/hooks/containerAnimation";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
 import "./style/about.scss";
 
 const About = ({ isOpen }: SectionProps) => {
   const t = useTranslations("about");
   const { open, isClosing, openModal, closeModal, handleAnimationEnd } =
     useToggleAnimation();
+
+  const titleRef = useRef<HTMLHeadingElement>(null);
+  const professionRef = useRef<HTMLParagraphElement>(null);
+  const aboutMeRef = useRef<HTMLParagraphElement>(null);
+
+  useEffect(() => {
+    if (open && !isClosing) {
+      gsap.set([titleRef.current, professionRef.current, aboutMeRef.current], {
+        opacity: 0,
+        y: 30,
+      });
+      const tl = gsap.timeline({
+        delay: 0.3,
+      });
+
+      tl.to(titleRef.current, {
+        opacity: 1,
+        y: 0,
+        duration: 0.8,
+        ease: "power3.out",
+      })
+        .to(
+          professionRef.current,
+          {
+            opacity: 1,
+            y: 0,
+            duration: 1.3,
+            ease: "power3.out",
+          },
+          "-=0.4"
+        )
+        .to(
+          aboutMeRef.current,
+          {
+            opacity: 1,
+            y: 0,
+            duration: 0.8,
+            ease: "power3.out",
+          },
+          "-=0.4"
+        );
+    }
+
+    if (isClosing) {
+      const tl = gsap.timeline();
+
+      tl.to(aboutMeRef.current, {
+        opacity: 0,
+        y: -20,
+        duration: 0.2,
+        ease: "power3.in",
+      })
+        .to(
+          professionRef.current,
+          {
+            opacity: 0,
+            y: -20,
+            duration: 0.7,
+            ease: "power3.in",
+          },
+          "-=0.2"
+        )
+        .to(
+          titleRef.current,
+          {
+            opacity: 0,
+            y: -20,
+            duration: 0.7,
+            ease: "power3.in",
+          },
+          "-=0.2"
+        );
+    }
+  }, [open, isClosing]);
 
   return (
     <div className="about">
@@ -28,9 +104,15 @@ const About = ({ isOpen }: SectionProps) => {
           onAnimationEnd={handleAnimationEnd}
         >
           <RiCloseLargeFill className="close" onClick={closeModal} />
-          <h2 className="about_me_title">{t("title")}</h2>
-          <p className="about_profecion">{t("profecion")}</p>
-          <p className="about_me">{t("about_me")}</p>
+          <h2 ref={titleRef} className="about_me_title">
+            {t("title")}
+          </h2>
+          <p ref={professionRef} className="about_profecion">
+            {t("profecion")}
+          </p>
+          <p ref={aboutMeRef} className="about_me">
+            {t("about_me")}
+          </p>
         </Container>
       )}
     </div>
