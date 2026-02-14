799bfffc8d47a4e4e445a3b4ae27a5eb1e1471cf
Giorgi014 <giorgi.gugunava99@gmail.com>
2026-02-14 23:52:38 +0400

    test: experimental changes to container reveal logic

--- src/app/[locale]/home/main/about/page.tsx
+++ src/app/[locale]/home/main/about/page.tsx
@@ -1,118 +1,19 @@
 "use client";
 
-import { Button, Container, HexBg } from "@/components";
+import { Button, HexBg } from "@/components";
 import { SectionProps } from "@/components/type";
 import { useTranslations } from "next-intl";
-import { RiCloseLargeFill } from "react-icons/ri";
-import { useToggleAnimation } from "@/app/hooks";
-import { useEffect, useRef } from "react";
-import gsap from "gsap";
 import "./style/about.scss";
 
 const About = ({ isOpen, onToggle }: SectionProps) => {
   const t = useTranslations("about");
-  const { open, isClosing, openModal, closeModal, handleAnimationEnd } =
-    useToggleAnimation({ isOpen, onToggle });
-
-  const titleRef = useRef<HTMLHeadingElement>(null);
-  const professionRef = useRef<HTMLParagraphElement>(null);
-  const aboutMeRef = useRef<HTMLParagraphElement>(null);
-
-  useEffect(() => {
-    if (open && !isClosing) {
-      gsap.set([titleRef.current, professionRef.current, aboutMeRef.current], {
-        opacity: 0,
-        y: 30,
-      });
-      const tl = gsap.timeline({
-        delay: 0.3,
-      });
-
-      tl.to(titleRef.current, {
-        opacity: 1,
-        y: 0,
-        duration: 0.8,
-        ease: "power3.out",
-      })
-        .to(
-          professionRef.current,
-          {
-            opacity: 1,
-            y: 0,
-            duration: 1.3,
-            ease: "power3.out",
-          },
-          "-=0.4",
-        )
-        .to(
-          aboutMeRef.current,
-          {
-            opacity: 1,
-            y: 0,
-            duration: 0.8,
-            ease: "power3.out",
-          },
-          "-=0.4",
-        );
-    }
-
-    if (isClosing) {
-      const tl = gsap.timeline();
-
-      tl.to(aboutMeRef.current, {
-        opacity: 0,
-        y: -20,
-        duration: 0.2,
-        ease: "power3.in",
-      })
-        .to(
-          professionRef.current,
-          {
-            opacity: 0,
-            y: -20,
-            duration: 0.7,
-            ease: "power3.in",
-          },
-          "-=0.2",
-        )
-        .to(
-          titleRef.current,
-          {
-            opacity: 0,
-            y: -20,
-            duration: 0.7,
-            ease: "power3.in",
-          },
-          "-=0.2",
-        );
-    }
-  }, [open, isClosing]);
 
   return (
     <div className="about">
-      <Button className={`about_btn ${isOpen && "active"}`} onClick={openModal}>
+      <Button className={`about_btn ${isOpen && "active"}`} onClick={onToggle}>
         <p className="about_title">{t("head")}</p>
         <HexBg className="about__bg" bgColor="#1C202B" borderColor="#000000" />
       </Button>
-
-      {open && (
-        <Container
-          variant="container"
-          className={`about_container ${isClosing && "closing"}`}
-          onAnimationEnd={handleAnimationEnd}
-        >
-          <RiCloseLargeFill className="close" onClick={closeModal} />
-          <h2 ref={titleRef} className="about_me_title">
-            {t("title")}
-          </h2>
-          <p ref={professionRef} className="about_profecion">
-            {t("profecion")}
-          </p>
-          <p ref={aboutMeRef} className="about_me">
-            {t("about_me")}
-          </p>
-        </Container>
-      )}
     </div>
   );
 };
--- src/app/[locale]/home/main/page.tsx
+++ src/app/[locale]/home/main/page.tsx
@@ -1,19 +1,32 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import About from "./about/page";
 import Contact from "./contact/page";
 import Projects from "./projects/page";
 import { ActiveSection, MainProps } from "@/components/type";
-import { HexBg } from "@/components";
+import { HexBg, Container } from "@/components";
 import { useTranslations } from "next-intl";
+import { RiCloseLargeFill } from "react-icons/ri";
+import { useToggleAnimation } from "@/app/hooks";
+import gsap from "gsap";
 import "./style/header.scss";
 
 const Header = ({ onProjectsToggle }: MainProps) => {
   const t = useTranslations("menu");
+  const tAbout = useTranslations("about");
   const [activeSection, setActiveSection] = useState<ActiveSection>(null);
   const [navigation, setNavigation] = useState<boolean>(false);
   const [isMobileNav, setIsMobileNav] = useState<boolean>(false);
+
+  const aboutToggle = useToggleAnimation({
+    isOpen: activeSection === "about",
+    onToggle: () => handleSectionToggle("about"),
+  });
+
+  const titleRef = useRef<HTMLHeadingElement>(null);
+  const professionRef = useRef<HTMLParagraphElement>(null);
+  const aboutMeRef = useRef<HTMLParagraphElement>(null);
 
   const handleSectionToggle = (section: ActiveSection) => {
     const newSection = activeSection === section ? null : section;
@@ -40,6 +53,76 @@ const Header = ({ onProjectsToggle }: MainProps) => {
 
     return () => window.removeEventListener("resize", handleResize);
   }, []);
+
+  useEffect(() => {
+    if (aboutToggle.open && !aboutToggle.isClosing) {
+      gsap.set([titleRef.current, professionRef.current, aboutMeRef.current], {
+        opacity: 0,
+        y: 30,
+      });
+      const tl = gsap.timeline({
+        delay: 0.3,
+      });
+
+      tl.to(titleRef.current, {
+        opacity: 1,
+        y: 0,
+        duration: 0.8,
+        ease: "power3.out",
+      })
+        .to(
+          professionRef.current,
+          {
+            opacity: 1,
+            y: 0,
+            duration: 1.3,
+            ease: "power3.out",
+          },
+          "-=0.4",
+        )
+        .to(
+          aboutMeRef.current,
+          {
+            opacity: 1,
+            y: 0,
+            duration: 0.8,
+            ease: "power3.out",
+          },
+          "-=0.4",
+        );
+    }
+
+    if (aboutToggle.isClosing) {
+      const tl = gsap.timeline();
+
+      tl.to(aboutMeRef.current, {
+        opacity: 0,
+        y: -20,
+        duration: 0.2,
+        ease: "power3.in",
+      })
+        .to(
+          professionRef.current,
+          {
+            opacity: 0,
+            y: -20,
+            duration: 0.7,
+            ease: "power3.in",
+          },
+          "-=0.2",
+        )
+        .to(
+          titleRef.current,
+          {
+            opacity: 0,
+            y: -20,
+            duration: 0.7,
+            ease: "power3.in",
+          },
+          "-=0.2",
+        );
+    }
+  }, [aboutToggle.open, aboutToggle.isClosing]);
 
   const toggleNavigation = () => {
     setNavigation(!navigation);
@@ -89,6 +172,28 @@ const Header = ({ onProjectsToggle }: MainProps) => {
             onToggle={() => handleSectionToggle("contact")}
           />
         </nav>
+      )}
+
+      {aboutToggle.open && (
+        <Container
+          variant="container"
+          className={`about_container ${aboutToggle.isClosing && "closing"}`}
+          onAnimationEnd={aboutToggle.handleAnimationEnd}
+        >
+          <RiCloseLargeFill
+            className="close"
+            onClick={aboutToggle.closeModal}
+          />
+          <h2 ref={titleRef} className="about_me_title">
+            {tAbout("title")}
+          </h2>
+          <p ref={professionRef} className="about_profecion">
+            {tAbout("profecion")}
+          </p>
+          <p ref={aboutMeRef} className="about_me">
+            {tAbout("about_me")}
+          </p>
+        </Container>
       )}
     </header>
   );
